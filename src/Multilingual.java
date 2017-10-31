a90da4d10c305a7af3360b8ab2626601ed6f8f43
Dov Kruger <Dov.Kruger@gmail.com>
2017-10-31 14:44:55 -0400

    first language example -- multinational code writing

---
+++ src/Multilingual.java
@@ -0,0 +1,8 @@
+public class Multilingual {
+	public static void main(String[] a) {
+		final double π = 3.14159265358979;
+		final double 大 = 1e20;
+		System.out.println(π);
+		System.out.println(大);
+	}
+}
